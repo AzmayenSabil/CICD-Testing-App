f3f7ec7f4745bc309c712ce60a2ca79009407667
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2024-05-09 23:59:09 +0600

    testing

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -1,7 +1,7 @@
 module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
     testMatch: [
-    //   '<rootDir>/test/**/unit.test.js',
+      '<rootDir>/test/**/unit.test.js',
       '<rootDir>/test/**/integration.test.js' // New pattern for integration tests
     ]
   };
