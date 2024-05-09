bdcc7f429516dd27ffa10a06ccd7b1cfae9b7e3e
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2024-05-09 23:27:17 +0600

    Update jest.config.unit.js

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -2,9 +2,6 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
     testMatch: [
       '<rootDir>/test/**/unit.test.js'
-    ],
-    setupFilesAfterEnv: [
-      '<rootDir>/test/setupTests.js'
     ]
   };
   
